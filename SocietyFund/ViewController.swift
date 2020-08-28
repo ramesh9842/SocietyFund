43cee623c5dbf30398a6e1b8ad446da333e0f939
Sanish C.K <ck.sanish@gmail.com>
2020-08-28 19:05:59 +0545

    Initial Commit

---
+++ SocietyFund/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocietyFund
+//
+//  Created by sanish on 8/28/20.
+//  Copyright © 2020 AahamSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
